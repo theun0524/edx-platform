eedacdb426ba8e82568bdefcf70dd704c49fc147
Taehwan Eun (은태환) <theun0524@gmail.com>
2018-12-06 13:58:17 +0900

    로그아웃시 access_token 쿠키도 같이 날린다.

--- lms/static/js/logout.js
+++ lms/static/js/logout.js
@@ -12,6 +12,8 @@
             $iframes = $iframeContainer.find('iframe'),
             redirectUrl = $iframeContainer.data('redirect-url');
 
+        /* remove access_token cookie for frontend */
+        $.cookie('access_token', null, { path: '/' });
         if ($iframes.length === 0) {
             window.localStorage.removeItem('session');
             window.location = redirectUrl;
